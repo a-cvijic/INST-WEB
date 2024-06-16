72f36ad7294b9ede94089eca0c74cb9bec3d5afc
acvijic <aleksa.cvijic@student.um.si>
2024-06-16 12:40:30 +0200

    Tailwind upd

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,17 @@ module.exports = {
         gray: colors.gray,
         slate: colors.slate,
       },
+      animation: {
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
+      keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+      },
     },
   },
   plugins: [
